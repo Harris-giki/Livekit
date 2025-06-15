ad8c7f9171b8e1cf1c8667efab408d63b8f08e6c
harris-giki <harris-giki@gmail.com>
2025-06-15 11:21:10 +0500

    Prompts Updated

--- prompts.py
+++ prompts.py
@@ -4,7 +4,6 @@ INSTRUCTIONS = """
     Start by collecting or looking up their car information. Once you have the car information, 
     you can answer their questions or direct them to the correct department.
 """
-
 WELCOME_MESSAGE = """
     Begin by welcoming the user to our auto service center and ask them to provide the VIN of their vehicle to lookup their profile. If
     they dont have a profile ask them to say create profile.
